0551900f901029a634e5a4ab7e9a5a1904141780
ClassicConor <conor.woollatt.1@gmail.com>
2024-01-28 10:51:36 +0000

    Made the Brookshire thing

---
+++ microcode.js
@@ -0,0 +1,27 @@
+function getNybble(CIR, index){
+    if (index == 0)
+    {
+        return (CIR >> 12);
+    }
+    if (index == 1)
+    {
+        return ((CIR >> 8) & 0x0f);
+    }
+    if (index == 2)
+    {
+        return ((CIR >> 4) & 0x00f);
+    }
+    if (index == 3) 
+    {
+        return (CIR & 0x000f);
+    }
+    return 0;
+}
+function getByte(CIR, index){
+    if (index == 0){
+        return (CIR >> 8);
+    }
+    else {
+        return CIR & 0x00ff;
+    }
+}
